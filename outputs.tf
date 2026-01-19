1d009df9b156f8892b5aee336634eade32bb0070
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-19 13:40:49 -0600

    Output.tf updated

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_public_ip" {
-    value = aws_instance.this.public_ip
+    value = aws_instance.Docker_EC2.public_ip
 }
 output "ec2_instance_id" {
-    value = aws_instance.this.id
+    value = aws_instance.Docker_EC2.id
   
 }
